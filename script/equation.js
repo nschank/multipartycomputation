387745db956e23a156f2e997517079642cf6cc24
nschank <nicolas.schank@gmail.com>
2014-06-13 00:14:27 -0400

    Fixed equations to use data (data-equation)

--- script/equation.js
+++ script/equation.js
@@ -2,50 +2,42 @@ window.addEventListener("load", addEquationListeners, true);
 
 function addEquationListeners()
 {
-	var counter = 0;
-	var equation = document.getElementById("equation"+counter);
-	while(equation != null)
-	{
-		addFunctionsE(equation,counter);
-		addDOME(equation, counter);
-		
-		counter++;
-		equation = document.getElementById("equation"+counter);
-	}
+    var elements = document.getElementsByClassName("equation");
+    for(var i = 0; i < elements.length; i++)
+    {
+            var elem = elements.item(i);
+            var num = elem.getAttribute("data-equation");
+            if(num !== null)
+		addDOME(elem, num);
+    }
 }
 
 function addDOME(equation,id)
 {
 	var block = document.createElement("div");
 	block.className="equationBlock";
-	block.id = "equation"+id+"block";
 	
 	block.innerHTML += "<h2>Equation Info</h2>";
 	block.innerHTML += fill(id);
 	block.innerHTML += "<p class=\"equationhelp\">For more help reading equations like these, visit our <a href=\"equations.html\">equations page</a></p>";
 	document.body.appendChild(block);
-	block.addEventListener('mouseover',keepOpenE);
-	block.addEventListener('mouseout',maybeCloseE);
+	block.addEventListener('mouseover',keepOpenE(block));
+	block.addEventListener('mouseout',maybeCloseE(block));
+        equation.addEventListener('mouseover',mouseOverListenerE(equation,block));
+	equation.addEventListener('mouseout',mouseOutListenerE(equation,block));
 }
 
-function addFunctionsE(equation,counter)
-{
-	equation.addEventListener('mouseover',mouseOverListenerE(equation,counter));
-	equation.addEventListener('mouseout',mouseOutListenerE(equation,counter));
-}
-
-function mouseOverListenerE(equation,counter)
+function mouseOverListenerE(equation,block)
 {
 	return function(event)
 	{
-		var thisBlock = document.getElementById("equation" + counter+"block");
-		thisBlock.style.display="block";
-		thisBlock.style.left=equation.getBoundingClientRect().right+"px";
-		thisBlock.style.top=equation.getBoundingClientRect().top+"px";
+		block.style.display="block";
+		block.style.left=equation.getBoundingClientRect().right+"px";
+		block.style.top=equation.getBoundingClientRect().top+"px";
 	};
 }
 
-function mouseOutListenerE(equation,counter)
+function mouseOutListenerE(equation,block)
 {
 	return function(event)
 	{
@@ -54,25 +46,26 @@ function mouseOutListenerE(equation,counter)
 		if(event.clientX > right && event.clientX < right+10)
 			if(top < event.clientY)
 				return;
-		var thisBlock = document.getElementById("equation" + counter+"block");
-		thisBlock.style.display="none";
+		block.style.display="none";
 	};
 }
 
-function keepOpenE(event)
+function keepOpenE(block)
 {
-	var thisBlock = event.target;
-	if(event.target.className == "equationBlock")
-		thisBlock.style.display="block";
+    return function(event)
+    {
+        block.style.display="block";
+    };		
 }
 
-function maybeCloseE(event)
+function maybeCloseE(block)
 {
-	var thisBlock = event.target;
-	var rect = thisBlock.getBoundingClientRect();
+    return function(event)
+    {
+        var rect = block.getBoundingClientRect();
 	if(rect.left < event.clientX && event.clientX < rect.right)
 		if(rect.top < event.clientY && event.clientY < rect.bottom)
 			return;
-	if(event.target.className == "equationBlock")
-		thisBlock.style.display="none";
+	block.style.display="none";
+    };
 }
