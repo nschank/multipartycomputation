60d9dc85886b87d37f5f8fd11946f8bfb3c56692
nschank <nicolas.schank@gmail.com>
2014-06-26 19:48:20 -0400

    NIterators initial commit

---
+++ src/nschank/util/NIterators.java
@@ -0,0 +1,30 @@
+package nschank.util;
+
+/**
+ * Created by Nicolas Schank for package nschank.util
+ * Created on 26 Jun 2014
+ * Last updated on 26 Jun 2014
+ *
+ * Utility class for Iterators
+ *
+ * @author nschank, Brown University
+ * @version 1.1
+ */
+public final class NIterators
+{
+	/**
+	 *
+	 */
+	private NIterators()
+	{
+		//Utility class
+	}
+
+	/**
+	 *
+	 */
+	public static void main(String[] args)
+	{
+
+	}
+}
