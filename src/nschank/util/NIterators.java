c2afb5b739a9cb63c7adb3db7c13268340641c61
nschank <nicolas.schank@gmail.com>
2014-06-26 20:00:17 -0400

    NIterators 1.2: subiterator

--- src/nschank/util/NIterators.java
+++ src/nschank/util/NIterators.java
@@ -1,5 +1,11 @@
 package nschank.util;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+
 /**
  * Created by Nicolas Schank for package nschank.util
  * Created on 26 Jun 2014
@@ -8,7 +14,7 @@ package nschank.util;
  * Utility class for Iterators
  *
  * @author nschank, Brown University
- * @version 1.1
+ * @version 1.2
  */
 public final class NIterators
 {
@@ -21,10 +27,54 @@ public final class NIterators
 	}
 
 	/**
-	 *
+	 * Given an Iterator and a Predicate of the same type, returns an iterator which goes through the given iterator but
+	 *  only returns those elements which satisfy the predicate.
+	 * @param universe
+	 * 		Any iterator
+	 * @param includeIf
+	 * 		A predicate which may return true for some elements of {@code universe}
+	 * @param <T>
+	 *     Any type
+	 * @return An iterator which goes through all elements of {@code universe} which satisfy {@code includeIf}, in same order
 	 */
-	public static void main(String[] args)
+	public static <T> Iterator<T> subiterator(final Iterator<T> universe, final Predicate<T> includeIf)
 	{
+		return new Iterator<T>()
+		{
+			private Optional<T> next = Optional.empty();
+			private boolean empty = false;
 
+			@Override
+			public synchronized boolean hasNext()
+			{
+				if(!next.isPresent()) step();
+				return !empty;
+			}
+
+			@Override
+			public synchronized T next()
+			{
+				if(!next.isPresent()) step();
+				if(empty) throw new NoSuchElementException();
+				return next.get();
+			}
+
+			/**
+			 *
+			 */
+			private synchronized void step()
+			{
+				while(universe.hasNext())
+				{
+					T possible = universe.next();
+					if(includeIf.test(possible))
+					{
+						next = Optional.of(possible);
+						return;
+					}
+				}
+				empty = true;
+			}
+		};
 	}
 }
