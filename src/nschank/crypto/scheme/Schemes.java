b87b37201f939b1dbb8ae1c74320e3a2f38cb879
nschank <nicolas.schank@gmail.com>
2014-06-27 12:21:39 -0400

    fixed RSA scheme to have a hardcore bit even among PRIMES

--- src/nschank/crypto/scheme/Schemes.java
+++ src/nschank/crypto/scheme/Schemes.java
@@ -14,7 +14,7 @@ import java.util.function.Function;
  * A Utility class for creating encryption/decryption schemes
  *
  * @author nschank, Brown University
- * @version 1.3
+ * @version 1.3.1
  */
 public final class Schemes
 {
@@ -45,7 +45,7 @@ public final class Schemes
 
 		return schemeBuilder((BigInteger b) -> b.modPow(e, n), (BigInteger b) -> b.modPow(d, n),
 				"(n=" + n.toString() + ",e=" + e.toString() + ")", "(d=" + d.toString() + ")",
-				(BigInteger b) -> b.mod(TWO).equals(BigInteger.ONE));
+				(BigInteger b) -> b.testBit(1));
 	}
 
 	/**
