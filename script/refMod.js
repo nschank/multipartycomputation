576e1d680f386ba88e91c694aa42e5ac4b946185
nschank <nicolas_schank@brown.edu>
2014-07-09 23:31:52 +0100

    updated version of referencing that should make life much easier and pages more extensible; not yet plugged in to anything

---
+++ script/refMod.js
@@ -0,0 +1,54 @@
+window.addEventListener("load", citeSources, true);
+
+function citeSources()
+{
+	var elements = document.getElementsByClassName("reference");
+	var toorder = {};
+	var inorder = [];
+	
+	for(var i = 0; i < elements.length; i++)
+	{
+		var elem = elements.item(i);
+		var cite = elem.getAttribute("data-citation");
+		var citedElem = document.getElementById(cite);
+		
+		if(!citedElem) continue;
+		if(toorder[cite] === undefined)
+			toorder[cite] = inorder.push(citedElem);
+		var link = "<a href=\"#" + cite + "\">[" + toorder[cite] + "]</a>";
+		elem.innerHTML = link;
+		addDOMR(elem.getAttribute("data-citation"), elem);
+	}
+	var list = document.getElementById("referencelist");
+	while(list.firstChild)
+		list.removeChild(list.firstChild);
+	inorder.forEach(function(elem) { list.appendChild(elem); });
+}
+
+function addDOMR(id, elem)
+{
+	var reference = document.getElementById(id);
+	if(reference === null) return false;
+	
+	var block = document.getElementById("ref_block"+id);
+	
+	if(block === null)
+	{
+		block = document.createElement("div");
+		block.className="ref_block";
+		block.id="ref_block"+id;
+		
+		block.innerHTML += "<h2>Reference</h2>";
+		block.innerHTML += "<p>"+reference.innerHTML+"</p>";
+		block.innerHTML += "<p><a href=\"" + authorLink(id) + "\">See more by this author</a></p>";
+	
+		block.innerHTML += "<p class=\"ref_goto_citations\"><a href=\"#referencelist\">See complete reference list</a></p>";
+		document.body.appendChild(block);
+		block.addEventListener('mouseover',keepOpen(block));
+		block.addEventListener('mouseout',maybeClose(block));
+		window.addEventListener('scroll', definitelyClose(block));
+	}
+    elem.addEventListener('mouseover',mouseOverListener(elem,block));
+	elem.addEventListener('mouseout',mouseOutListener(elem,block));
+	return true;
+}
