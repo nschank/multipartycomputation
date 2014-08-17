5e7fc60971ca4ebf334168a690cf0da9569f7ed2
nschank <nicolas_schank@brown.edu>
2014-08-18 00:23:43 +0100

    Fully updated TOC and changed around how it reads the HTML, updating the pages s needed. Some reformatting needs to be done.

--- script/toc.js
+++ script/toc.js
@@ -1,62 +1,73 @@
 window.addEventListener("load", fillTOC, true);
 
-var headings = ["H1","H2","H3","H4","H5"];
-var headingName = /[\d\.]*\s*(.+?)[\.:]?$/;
+var re = /<h(\d)>[\d\.]*\s*(.+?)<\/h\1>/; 
 
+/**
+ * Finds all divs of class "main_toc", and fills them with a table of contents built from the section tags within the document.
+ */
 function fillTOC()
 {
 	var table = buildTable();
 	console.log(table);
-	/*var ol = document.createElement("ol");
+	
+	var ol = document.createElement("ol");
 	ol.setAttribute('type', 'A');
 	ol = buildNode(table,ol);
+	
+	var h4 = document.createElement("h4");
+	h4.innerHTML = "<h4 style=\"color:#333333\">Table of Contents</h4>";
 	
 	var tocs = document.getElementsByClassName("main_toc");
 	
 	for(var i = 0; i < tocs.length; i++)
 	{
 		var toc = tocs.item(i);
-		toc.innerHTML = "<h4 style=\"color:#333333\">Table of Contents</h4>";
+		toc.appendChild(h4.cloneNode(true));
 		toc.appendChild(ol.cloneNode(true));
 	}
-	
-	addNav(table);*/
+	console.log(table);
+	addNav(table);
 }
 
+/**
+ * Searches all elements with class top_section and the children thereof to build a table of contents table.
+ */
 function buildTable()
 {
-	var sections = document.getElementsByClassName("main_section");
+	var sections = document.getElementsByClassName("top_section");
 	var table = [];
 	
 	for(var i = 0; i < sections.length; i++)
 	{
-		console.log(i);
 		var section = sections.item(i);
 		var sectionName = name(section);
-		console.log(sectionName);
 		if(!sectionName)
 			continue;
 		var children = subs(section);
-		console.log(table);
 		
 		table.push({name:sectionName, id:section.id, children:children});
 	}
 	return table;
 }
 
+/**
+ * The name of a section is preferably the contents of its data-section-name attribute, or barring that, the contents of the first heading within it.
+ */
 function name(section)
 {
 	var name = section.getAttribute("data-section-name")
 	if(name)
 		return name;
-	var re = /<h(\d)>[\d\.]*\s*(.+?)<\/h\1>/; 
 	var str = section.innerHTML;
 	var m = re.exec(str);
 	
 	if(!m) return null;
 	return m[2];
 }
-
+ 
+/**
+ * Searches all the children of a container for sections with names and ids.
+ */
 function subs(container)
 {
 	var table = [];
@@ -81,6 +92,9 @@ function subs(container)
 	return table;
 }
 
+/**
+ * Builds the table of contents list from the table
+ */
 function buildNode(table, list)
 {
 	for(var i = 0; i < table.length; i++)
@@ -88,7 +102,7 @@ function buildNode(table, list)
 		var li = document.createElement("li");
 		var link = document.createElement("a");
 		link.setAttribute("href","#" + table[i].id);
-		link.innerHTML = headingName.exec(table[i].name)[1];
+		link.innerHTML = table[i].name;
 		
 		var num;
 		if(num = document.getElementById(table[i].id).getAttribute('data-section-number'))
@@ -102,26 +116,44 @@ function buildNode(table, list)
 	return list;
 }
 
+/**
+ * Iterates through the table and adds a "current section", "prev section", and "next section" link at every id in the table.
+ */
 function addNav(table)
 {
 	var cursor = {last:undefined, current:undefined};
 	addNavHelper(cursor, table, 0);
-	addNavDiv(cursor.current, cursor.last, undefined);
+	//addNavHelper misses the last element
+	addNavDiv(cursor.last, cursor.current, undefined);
 }
 
+/**
+ * The current level being iterated at is objectArray. Children are checked first, since "next" is depth-first. cursor is the current
+ * cursor object. Index is the index within the objectArray where the next element at the current level is located.
+ */
 function addNavHelper(cursor, objectArray, index)
 {
 	if(!objectArray || objectArray.length <= index)
 		return;
+	
+	//Add navigation points if there is a current element
 	if(cursor.current)
-		addNavDiv(cursor.current, cursor.last, objectArray[index]);
+		addNavDiv(cursor.last, cursor.current, objectArray[index]);
+		
+	//Iterate to next element
 	cursor.last = cursor.current;
 	cursor.current = objectArray[index];
+	//Check children first
 	addNavHelper(cursor, cursor.current.children, 0);
+	//Then do next at this level
 	addNavHelper(cursor, objectArray, index+1);
 }
 
-function addNavDiv(current, last, next)
+/**
+ * Adds the navigation buttons to a section. Last, next, and current are as expected. The navigation buttons are added to an rp_linkbox that is added as the
+ * first child within document.getElementById(current.id)
+ */
+function addNavDiv(last, current, next)
 {
 	if(!current) return;
 	var div = document.createElement("div");
@@ -132,5 +164,6 @@ function addNavDiv(current, last, next)
 	if(next)
 		html += "&nbsp;<a href=\"#" + next.id + "\" title=\"Next section\">&#8595;</a>";
 	div.innerHTML = html;
-	current.nameElement.parentNode.insertBefore(div, current.nameElement);
+	var elem = document.getElementById(current.id);
+	elem.insertBefore(div, elem.firstChild);
 }
